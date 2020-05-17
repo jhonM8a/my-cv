df680d010472d02a9f719d781de1b3c7dab9350b
jhonochoa <jhon.ochoa@unillanos.edu.co>
2020-05-17 13:32:33 -0500

    Updates

---
+++ precache-manifest.8fd248ef57d43a24db35d82f76d28a23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b995eacee1811acca797d4c4c493987d",
+    "url": "/jhonM8a/my-cv/index.html"
+  },
+  {
+    "revision": "4140b90d31a43532fb03",
+    "url": "/jhonM8a/my-cv/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "612fb74c6943181db82f",
+    "url": "/jhonM8a/my-cv/static/css/main.6e8cdaa6.chunk.css"
+  },
+  {
+    "revision": "4140b90d31a43532fb03",
+    "url": "/jhonM8a/my-cv/static/js/2.5e8977d8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jhonM8a/my-cv/static/js/2.5e8977d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "612fb74c6943181db82f",
+    "url": "/jhonM8a/my-cv/static/js/main.2952ac9e.chunk.js"
+  },
+  {
+    "revision": "344ef4459518e35ccccd",
+    "url": "/jhonM8a/my-cv/static/js/runtime-main.93a139f1.js"
+  }
+]);
