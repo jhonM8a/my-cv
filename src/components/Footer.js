f42add6bc160a98213d50f04108c325ca55b567c
jhonM8a <jhon.ochoa@unillanos.edu.co>
2020-05-25 15:31:07 -0500

    Se tiene en cuenta tamños de pantallas

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,29 +1,40 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
-function Footer(props){
-    return(
-        <div>
-            <section className="py-5">
-                <div className="container">
-                    <h2 className="font-weight-light text-center font-weight-bold">Info contact</h2>
-                    <div className="row">
-                        <div className="col-8">
-                            <ul>
-                                <li>Email: jhon.mario.8a@gmail.com</li>
-                                <li>Ubicación: Cali - Colombia</li>
-                            </ul>
-                        </div>
-                        <div className="col-4">
-                            <li>Linkedin: <Link to="https://www.linkedin.com/in/jhon-mario-ochoa-lemus-3132b716a">Jhon Mario Ochoa</Link></li>
-                            <li>Github: <Link to="https://github.com/jhonM8a/">JhonM8a</Link></li>
-                        </div>
-                    </div>
-
-                </div>
-            </section>
+function Footer(props) {
+  return (
+    <div>
+      <section className="py-5">
+        <div className="container">
+          <h2 className="font-weight-light text-center font-weight-bold">
+            Info contact
+          </h2>
+          <div className="row">
+            <div className="col-xs-12 col-md-8">
+              <li>Email: jhon.mario.8a@gmail.com</li>
+              <li>Ubicación: Cali - Colombia</li>
+              <li>Tel: (+57) 3116192217</li>
+            </div>
+            <div className="col-xs-12 col-md-4">
+              <li>
+                Linkedin:{" "}
+                <Link to="https://www.linkedin.com/in/jhon-mario-ochoa-lemus-3132b716a">
+                  Jhon Mario Ochoa
+                </Link>
+              </li>
+              <li>
+                Github: <Link to="https://github.com/jhonM8a/">JhonM8a</Link>
+              </li>
+              <li>
+                Twitter:{" "}
+                <Link to="https://twitter.com/Jhon83777065">@Jhon83777065</Link>
+              </li>
+            </div>
+          </div>
         </div>
-    )
+      </section>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
